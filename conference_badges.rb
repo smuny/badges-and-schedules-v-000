d2e91fe1b4f23c6cd55b159ed7aa93687015d0e1
Sharon Mun <sharonymun@gmail.com>
2018-06-09 17:33:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require "pry"
+attendees =  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|    #.map will print multiple elements in the argument in the phrase that is below. attendees = "attendee, "attendee, "attendee, "attendee"
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(attendees)
+  array = []
+attendees.each_with_index do |attendee, room_assignments|
+  array << "Hello, #{attendee}! You'll be assigned to room #{room_assignments += 1}!"
+end
+return array
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |room_assignments|
+    puts room_assignments
+  end
+end
